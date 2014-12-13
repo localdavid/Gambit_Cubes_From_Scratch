8411686698612a0a3c32f43b788a25f284ce4fee
David Kroll <david.s.kroll@gmail.com>
2014-12-13 11:17:15 -0600

    Filter to remove non headliner
    Goes through all divs with class=band. Those that don't have class=headliner are removed.

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 	$(".button").click(function() {
 		//Filter to show only headliner
-		$("div .band").not(".headliner").css("background-color", "black");
+		// Goes through all divs with class=band. Those that don't have class=headliner are removed.
+		$("div .band").not(".headliner").remove();
 	});
 });
 
