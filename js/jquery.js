c93df2abc9677bbf99cc0c5eef54f82b593c1f17
David Kroll <david.s.kroll@gmail.com>
2014-12-14 14:06:09 -0600

    Haven't saved in a while

--- js/jquery.js
+++ js/jquery.js
@@ -6,9 +6,15 @@ $(document).ready(function() {
 	});
 	
 	$(".alex_button").click(function() {
-		//Filter to show only headliner
+		//Filter to show only alex's picks
 		// Goes through all divs with class=band. Those that don't have class=headliner are toggled on and off
 		$("div .band").not(".alex").toggle();
+	});
+		
+	$(".cover_button").click(function() {
+		//Filter to show only cover story subjects
+		// Goes through all divs with class=band. Those that don't have class=headliner are toggled on and off
+		$("div .band").not(".cover").toggle();
 	});
 });
 
