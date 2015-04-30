45f7fbbf3780f1c5def5f1755d255387b52c2cb2
David Kroll <david.s.kroll@gmail.com>
2015-04-29 21:01:55 -0500

    Click on Basin button when PPicks is active, PPicks deactivates

--- js/jquery.js
+++ js/jquery.js
@@ -13,33 +13,33 @@ $(document).ready(function() {
 
 
 
-$(document).ready(function() {
-	
-	// Define the open and close submenu functions
-/*
+	$(document).ready(function() {
+		
+		// Define the open and close submenu functions
+	/*
+				function openSubMenu() {
+				$(this).find('ul').css('visibility', 'visible');	
+			}
+			
+			function closeSubMenu() {
+				$(this).find('ul').css('visibility', 'hidden');	
+			}
+	*/
+		
+		// when the mouse is moved over a li in myMenu, the function openSubMenu is called
+			$('.myMenu > li').on('mouseover', openSubMenu);
+		//  We need to bind another event listener to detect when the mouse rolls out and call a function that gets rid of the dropdown.
+			$('.myMenu > li').on('mouseout', closeSubMenu);
+		
+		// When the mouse rolls over the list item, the function looks for an unordered list within it. If one is found, it sets the style property visibility to visible.
 			function openSubMenu() {
-			$(this).find('ul').css('visibility', 'visible');	
-		}
-		
-		function closeSubMenu() {
-			$(this).find('ul').css('visibility', 'hidden');	
-		}
-*/
-	
-	// when the mouse is moved over a li in myMenu, the function openSubMenu is called
-		$('.myMenu > li').on('mouseover', openSubMenu);
-	//  We need to bind another event listener to detect when the mouse rolls out and call a function that gets rid of the dropdown.
-		$('.myMenu > li').on('mouseout', closeSubMenu);
-	
-	// When the mouse rolls over the list item, the function looks for an unordered list within it. If one is found, it sets the style property visibility to visible.
-		function openSubMenu() {
-			$(this).find('ul').css('visibility', 'visible');	
-		}
-		
-		function closeSubMenu() {
-			$(this).find('ul').css('visibility', 'hidden');	
-		}
-	});
+				$(this).find('ul').css('visibility', 'visible');	
+			}
+			
+			function closeSubMenu() {
+				$(this).find('ul').css('visibility', 'hidden');	
+			}
+		});
 
 
 
@@ -66,12 +66,20 @@ $(document).ready(function() {
 		$(document).ready(function() {
 			$(".count_basin_button").click(function() {
 				
-				// Button set to active state
-				$(this).toggleClass("basin_button_active");
+				// Activate button itself
 				
-				// Console log
-				console.log("Count Basin button activated");
+					// Button set to active state
+					$(this).toggleClass("basin_button_active");
+					
+					// Console log
+					console.log("Count Basin button activated");
+					
+				// De-activate any other active buttons
 				
+					    if ($(".performer_pick_button").hasClass("performer_pick_button_active") === true) {
+						    $(".performer_pick_button").toggleClass("performer_pick_button_active");
+						};
+					
 				// Hide non Basin pick cubes
 				$(".cube_column").children("div").not(".band_box_basin_pick").fadeToggle(200);
 			});
@@ -115,35 +123,7 @@ $(document).ready(function() {
 
 
 
-
-// old code
-
-		
-/*
-			// For Inactive Basin button (one that isn't already clicked)
-				// Press Basin button
-				$(".count_basin_button").click(function() {
-					console.log("Basin button clicked");
-					
-					// Activate Basin button
-					// Basin button now in active state by changing class of .count_basin_button
-					$(this).toggleClass('count_basin_button_clicked');
-					
-					// Set other button to inactive
-					$(".performer_pick_button_clicked").removeClass('performer_pick_button');
-					console.log("Other buttons set to inactive");
-					
-					//Show all divs before hiding the right ones
-					$(".cube_column").children("div").show();
-					console.log("Show all divs before hiding the right ones");				
-					
-					// Hide divs that aren't Basin Picks
-						// via: Selects only divs that are direct descendents of .cube_column and that aren't .band_box_basin_pick (Count Basins picks)
-						$(".cube_column").children("div").not(".band_box_basin_pick").fadeToggle(200);
-				});
-				
-*/
-				 
+		 
 
 
 // Sticky navigation
